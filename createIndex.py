bd5aa080a8f03c87352cec9f985674cca7e61cdb
Tyler Vu <tylerkienvu@gmail.com>
2018-05-15 12:36:46 -0700

    Deleted db script and added more to createIndex
    Database script no longer needed because we are going to be storing the inverted index on a file

--- createIndex.py
+++ createIndex.py
@@ -2,6 +2,8 @@
 to travel through the directory of HTMl files and create the index"""
 
 import json
+import os
+from collections import defaultdict
 from pprint import pprint
 
 def openJsonFile(path):
@@ -9,6 +11,18 @@ def openJsonFile(path):
         data = json.load(f)
         return data
 
+#traverses through every file in the webpages directories
+def traverseDirectory(path):
+    for root,dirs, files in os.walk(path):
+        for file in files:
+            if( file in ["bookkeeping.json","bookkeeping.tsv"]):
+                continue
+            # this is where you would start creating the index
+            #appendToIndex(os.path.join(root,file))
+            print( os.path.join(root, file))
+
+def appendToIndex(path):
+    pass
 
 
 
@@ -16,4 +30,4 @@ def openJsonFile(path):
 if __name__ == "__main__":
     # Change file path to path on your computer
     data = openJsonFile(r"C:\Users\tyler\Documents\GitHub\webpages\WEBPAGES_RAW\bookkeeping.json")
-    print(data["0/0"])
+    traverseDirectory(r"C:\Users\tyler\Documents\GitHub\webpages\WEBPAGES_RAW")
